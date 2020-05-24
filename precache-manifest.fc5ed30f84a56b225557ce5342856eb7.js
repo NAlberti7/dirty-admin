ce1ca026a0aaf4ef28e560d4166dce3f98ddc778
Nahuel Alberti <40583848+NAlberti7@users.noreply.github.com>
2020-05-23 23:55:53 -0300

    Add files via upload

---
+++ precache-manifest.fc5ed30f84a56b225557ce5342856eb7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4801bf89e83f71fc544260749530e202",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eae8da4d46a03b8c5956",
+    "url": "/static/css/main.a2cca5ac.chunk.css"
+  },
+  {
+    "revision": "700b6504f0cb5ce0f905",
+    "url": "/static/js/2.df1d901b.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.df1d901b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eae8da4d46a03b8c5956",
+    "url": "/static/js/main.b56d5c40.chunk.js"
+  },
+  {
+    "revision": "fbb6d3afdf3603079f35",
+    "url": "/static/js/runtime-main.e00884a7.js"
+  },
+  {
+    "revision": "a13695c5fc388a9fe2672b28bf1a5d10",
+    "url": "/static/media/MonumentExtended-Ultrabold.a13695c5.woff"
+  },
+  {
+    "revision": "723e6bad7bb3e4b87e947416dce765ad",
+    "url": "/static/media/RobotoMono-Regular.723e6bad.woff"
+  },
+  {
+    "revision": "42119ea4e85fda16c520d6ba00b6e5af",
+    "url": "/static/media/logo.42119ea4.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4aaf92d0ed86d1b1165581081977af0d",
+    "url": "/static/media/open-menu.4aaf92d0.svg"
+  }
+]);
